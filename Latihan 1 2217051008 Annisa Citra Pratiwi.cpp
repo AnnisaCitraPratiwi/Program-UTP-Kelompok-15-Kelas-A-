e4b38e6b538bc78dea11f4761bc981072b3eeb51
Annisa Citra Pratiwi <112589538+AnnisaCitraPratiwi@users.noreply.github.com>
2022-09-09 10:17:37 +0700

    Add files via upload

---
+++ Latihan 1 2217051008 Annisa Citra Pratiwi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float pi,r,luas = 3.14;
+	
+	cout<<"menghitung luas lingkaran\n"; 
+	cout<<"masukan jari jarinya\n"; 
+	cin>>r;
+	luas=pi*r*r;
+	cout<<"luas lingkarannya adalah :"<<luas;
+	
+	return 0; 
+}
